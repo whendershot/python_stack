9e7e511044e200bc1bbc79fd51277438ffc95a0c
William Hendershot <whendershot1@gmail.com>
2021-12-05 12:32:16 +0900

    Fixed remove_value Updated test block. {TODO} Change to unit testing. {TODO} Finish insert_at function {TODO} Implement reverse

--- doubly_linked_list/dlist.py
+++ doubly_linked_list/dlist.py
@@ -81,13 +81,46 @@ class DList:
         while (pointer.next and pointer.next.value != val):
             pointer = pointer.next
         
-        result = pointer.next.value
-        pointer.next = pointer.next.next
-        return result
+        #I am either at the node just before or at the end of the list
+        if pointer.next:
+            result = pointer.next.value
+            pointer.next = pointer.next.next
+            pointer.next.prev = pointer
+            self.length -= 1
+            return result
+        raise IndexError(f"Value ({val}) does not exist in the list.")
 
 
     def insert_at(self, val, n):
-        pass
+        if n < 0:
+            raise IndexError("n must be 0 or a positive integer.")
+
+        if self.length == 0 or n == 0:
+            self.add_to_front(val)        
+        elif n >= self.length:
+            self.add_to_back(val)
+        else:            
+            midpoint = int(self.length / 2)
+            #start from front
+            if n < midpoint:
+                pointer = self.head
+                n -= 1
+                while n > 0:
+                    pointer = pointer.next
+                    n -= 1
+                
+            #start from tail
+            else:
+                pointer = self.tail
+                for i in range(self.length - 1, n, -1):
+                    pointer = pointer.prev
+
+            new_node = DLNode(val)
+            new_node.next = pointer.next
+            new_node.prev = pointer
+            pointer.next = new_node
+
+        return self
 
     def reverse(self):
         pass
@@ -97,13 +130,13 @@ if __name__ == "__main__":
     my_list = DList()
     my_list.add_to_front("are").add_to_front("Linked lists").add_to_back("fun!").print_values()
 
-    my_list.remove_from_front()
+    print(f"Removing: {my_list.remove_from_front()}")
     my_list.print_values()
 
-    my_list.remove_from_back()
+    print(f"Removing: {my_list.remove_from_back()}")
     my_list.print_values()
 
-    my_list.remove_from_back()
+    print(f"Removing: {my_list.remove_from_back()}")
     try:
         my_list.print_values()
     except IndexError:
@@ -114,19 +147,19 @@ if __name__ == "__main__":
     my_list.print_values()
 
     my_list.add_to_back("I'm a little teapot")
-    my_list.remove_value(42)
+    print(f"Removing: {my_list.remove_value(42)}")
     my_list.print_values()
 
     try:
-        my_list.remove_value(42)
+        print(f"Trying to remove 42: {my_list.remove_value(42)}")
     except IndexError:
         print("Oops! I already took that one out!")
     my_list.print_values()
 
-    my_list.remove_value("The universe: ")
+    print(f'''Removing: {my_list.remove_value("The universe: ")}''')
     my_list.print_values()
 
-    my_list.remove_value("I'm a little teapot")
+    print(f'''Removing: {my_list.remove_value("I'm a little teapot")}''')
     try:
         my_list.print_values()
     except IndexError:
